8c1a85b1d18da5e2d78b922a7240795b9174950d
alepaez-dev <alepaezdev@gmail.com>
2022-07-11 20:02:36 -0600

    feat: middleware

--- src/routes/koder.route.js
+++ src/routes/koder.route.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const app = require("../server");
 const { getAll, getById, create } = require("../usecases/koder.usecase")
--- src/server.js
+++ src/server.js
@@ -1,13 +1,31 @@
+/**
+ * Ejercicio 1
+ * Hacer un middleware global, que me imprima
+ * EJ: 
+ * 
+ * enndpoint request.method
+ * 
+ * console.log("method", "estamos por un middleware")
+ * - GET: "Estamos pasando por un middleware"
+ * 
+ * Ejercicio 2
+ * Hacer PATCH y DELETE de koder.
+ */
+
 const express = require("express")
 const routerKoders = require("./routes/koder.route")
+const middlewareImprimir = require("./middlewares/generico.middleware")
 
 const app = express();
 
 // Middleware
 app.use(express.json())
+app.use(middlewareImprimir)
 
 // Middleware de ruta
 app.use("/koders", routerKoders)
+
+
 
 /**
  * 
